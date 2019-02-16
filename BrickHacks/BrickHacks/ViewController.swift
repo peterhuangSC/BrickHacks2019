6e9fb423fae30b39aec030154271e379f265afc4
Peter Huang <peter.huang@enflick.com>
2019-02-16 12:59:53 -0500

    App base built

---
+++ BrickHacks/BrickHacks/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BrickHacks
+//
+//  Created by Peter Huang on 2019-02-16.
+//  Copyright © 2019 Peter Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //test app
+        
+    }
+
+
+}
+
